8f661eb66589d0e323b1ef2ef0b78858096d407f
Amanda Slayton <alslayton86@gmail.com>
2018-03-26 21:15:36 -0500

    add XHR request to JS

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log("Why, hello there");
+// console.log("Why, hello there");
+
+const printToDom = (myInnerds, putItHere) => {
+  document.getElementById(putItHere).innerHTML = myInnerds;
+};
+
+function ifItLoads(){
+  const myData = JSON.parse(this.responseText);
+  buildDomString(myData.animals);
+};
+
+function WTF() {
+  console.log("You Suck");
+};
+
+const buildDomString = (animalArray) => {
+  let myString = "";
+  for(let i = 0; i < animalArray.length; i++){
+    myString += `<h1>${animalArray[i].names}<h1>`;
+  };
+  
+  printToDom(myString, "animal-container")
+};
+
+function startApplication(){
+  let myRequest = new XMLHttpRequest();
+  myRequest.addEventListener("load", ifItLoads);
+  myRequest.addEventListener("error", WTF);
+  myRequest.open("GET", "animals.json");
+  myRequest.send();
+}; 
+
+startApplication();
